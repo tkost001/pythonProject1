839ab207fc3ee5da1ec08de8e36abb6df323aef9
tkost001 <tkost001@gmail.com>
2022-06-09 22:16:45 -0500

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject1',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='tomislavkosta',
+    author_email='',
+    description=''
+)
